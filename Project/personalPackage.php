29315fd466d6feaaa7cc85265d7f7c6ec788ecba
dmorto24 <123219140+dmorto24@users.noreply.github.com>
2023-04-26 15:20:11 -0500

    Add files via upload

--- Project/personalPackage.php
+++ Project/personalPackage.php
@@ -9,15 +9,22 @@ $uid=$_SESSION['id'];?>
 </head>
 <body>
 <table align="center">
-<form method="post" action="finalize.php">
+
 <?php	
 	$sql2="SELECT * FROM package_table WHERE user_id='$uid' ";
 	$result2=$connect->query($sql2);
+	if(mysqli_num_rows($result2) == 0){
+		echo "<h2> You haven't added anything to your package yet </h2>";
+		echo "<a href='display_hotels.php'>Click here to start building your dream vacation!</a></td>";
+	}
+	else{
 	$count=1;
 	$total=1;
 	$ACTprice=0;
 	$CARprice=0;
 	$HOTprice=0;
+	$RESprice=0;
+	echo "<h2>This Vacation is Uniquely Yours</h2>";
 	echo "<table align='center' class='hotelTable'>";
 	echo "<tr>";
 	while($row2 = $result2->fetch_assoc())
@@ -35,6 +42,7 @@ $uid=$_SESSION['id'];?>
 				echo "<br>";
 				echo $rowACT['name']."<br>";
 				echo "$".$rowACT['price']." per person<br>";
+				echo "<form action='removefrompackage.php' method='post'><button type='submit' name='remove' id='car".$total."' value='".$rowACT['name']."'>Remove from Package</button></form>";
 			echo "</td>";
 			$count++;
 			$total++;
@@ -49,6 +57,7 @@ $uid=$_SESSION['id'];?>
 				echo "<br>";
 				echo $rowCAR['type']."<br>";
 				echo "$".$rowCAR['price']."/day<br>";
+				echo "<form action='removefrompackage.php' method='post'><button type='submit' name='remove' id='car".$total."' value='".$rowCAR['type']."'>Remove from Package</button></form>";
 			echo "</td>";
 			$count++;
 			$total++;
@@ -63,17 +72,36 @@ $uid=$_SESSION['id'];?>
 				echo "<br>";
 				echo $rowHOT['name']."<br>";
 				echo "$".$rowHOT['price']."/night<br>";
+				echo "<form action='removefrompackage.php' method='post'><button type='submit' name='remove' id='car".$total."' value='".$rowHOT['name']."'>Remove from Package</button></form>";
 			echo "</td>";
 			$count++;
 			$total++;
 			$HOTprice+=$rowHOT['price'];
-		}	
+		}
+		else if($row2['typeadded']=='R'){
+			$sqlRES="SELECT * FROM resturant_table WHERE name='".$row2['added']."'";
+			$resultRES=$connect->query($sqlRES);
+			$rowRES = $resultRES->fetch_assoc();
+			echo "<td>";
+				"<img height='400px' width='500px' src= '".$rowRES['img'].".jpg'/>";
+				echo "<br>";
+				echo $rowRES['name']."<br>";
+				echo "$".$rowRES['price']."/night<br>";
+				echo "<form action='removefrompackage.php' method='post'><button type='submit' name='remove' id='car".$total."' value='".$rowRES['name']."'>Remove from Package</button></form>";
+			echo "</td>";
+			$count++;
+			$total++;
+			$RESprice+=$rowRES['price'];
+		}
 	}
 	echo "</table>";?>
+<form method="post" action="finalize.php">
 	<input type="hidden" name="ACTprice" value="<?php echo $ACTprice; ?>">
 	<input type="hidden" name="CARprice" value="<?php echo $CARprice; ?>">
 	<input type="hidden" name="HOTprice" value="<?php echo $HOTprice; ?>">
+	<input type="hidden" name="RESprice" value="<?php echo $RESprice; ?>">
 	<button type="submit">Continue</button>
 </form>	
+<?php }?>
 </body>
 </html>
